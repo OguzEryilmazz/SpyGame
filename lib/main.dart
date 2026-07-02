332925d353948be3d960b53bd9f853da25e30d6a
Mustafa Eryılmaz <mustafaerylmzz@gmail.com>
2026-07-02 23:06:08 +0300

    added nsu permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'router.dart';
 import 'billing/iap_service.dart';
-import 'ads/banner_ad_widget.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await AppTrackingTransparency.requestTrackingAuthorization();
+
+  await _initConsent();
+
   await MobileAds.instance.initialize();
   await IAPService().initialize();
   runApp(
@@ -14,6 +21,23 @@ void main() async {
       child: SpyApp(),
     ),
   );
+}
+
+Future<void> _initConsent() async {
+  final completer = Completer<void>();
+  ConsentInformation.instance.requestConsentInfoUpdate(
+    ConsentRequestParameters(),
+    () async {
+      if (await ConsentInformation.instance.isConsentFormAvailable()) {
+        final innerCompleter = Completer<void>();
+        ConsentForm.loadAndShowConsentFormIfRequired((_) => innerCompleter.complete());
+        await innerCompleter.future;
+      }
+      completer.complete();
+    },
+    (error) => completer.complete(),
+  );
+  return completer.future;
 }
 
 class SpyApp extends ConsumerWidget {
