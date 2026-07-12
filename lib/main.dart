4036712bf51db4e16f67212f77c14f89ad188198
Oguz <oguzeryilmaz4@gmail.com>
2026-07-12 22:31:27 +0300

    Unlock subcategories via rewarded ads, enable banner ad, add AdMob App ID to Android manifest

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'router.dart';
 import 'billing/iap_service.dart';
+import 'ads/banner_ad_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -75,7 +76,7 @@ class _SpyAppState extends ConsumerState<SpyApp> {
         return Column(
           children: [
             Expanded(child: child ?? const SizedBox.shrink()),
-            // const BannerAdWidget(),
+             const BannerAdWidget(),
           ],
         );
       },
